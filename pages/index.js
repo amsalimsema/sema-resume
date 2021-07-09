a2fa1b614dfccd429dc1caf27a45d04f53765019
Salim Semakula <salimsema@yahoo.com>
2021-07-09 14:16:40 +0300

    nextstart

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,61 @@
 import Head from "next/head";
 import Image from "next/image";
+// import styled from "styled-components";
+import { Row, Col } from "react-bootstrap";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Portfolio</title>
         <meta name="description" content="Personal Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.outer}>
+        <Image src="/apercu.jpg" alt="dooo" layout="fill" />
+        <div className={styles.content}>hallelu</div>
+      </div>
+      <div className={styles.Home}>
+        <div className={styles.outer}>
+          <Image src="/principles.jpg" alt="dooo" layout="fill" />
+          <div className={styles.content}>hallelu</div>
         </div>
-      </main>
+        <div className={styles.info_wrap}>
+          <p className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iusto
+            quasi quis, perferendis unde dolor vero placeat doloremque dolorum
+            cupiditate commodi velit, incidunt modi illo harum necessitatibus
+            architecto nisi aliquam!
+          </p>
+        </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+        <div className={styles.info_wrap}>
+          <p className={styles.info}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iusto
+            quasi quis, perferendis unde dolor vero placeat doloremque dolorum
+            cupiditate commodi velit, incidunt modi illo harum necessitatibus
+            architecto nisi aliquam!
+          </p>
+        </div>
+        <div className={styles.outer}>
+          <Image src="/me.jpg" alt="dooo" layout="fill" />
+          <div className={styles.content}>hallelu</div>
+        </div>
+      </div>
+
+      <div className={styles.box}>
+        <Row className="no-gutters">
+          <Col md={6}>
+            <div className={styles.outer}>
+              <Image src="/me.jpg" alt="dooo" layout="fill" />
+              <div className={styles.content}>hallelu</div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 }
